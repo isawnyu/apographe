1e753c22b1a61b12508a75dd2bf9313dec56418c
Tom Elliott <ipse@paregorios.org>
2022-03-10 14:23:51 -0600

    setup and package metadata

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
+#
+# This file is part of "apographe"
+# by Tom Elliott for the Institute for the Study of the Ancient World
+# (c) Copyright 2022 by New York University
+# Licensed under the AGPL-3.0; see LICENSE.txt file.
+#
+
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="{distribution-name}",
+    name="apographe",
     version="0.0.1",
-    author="{author}",
-    author_email="{author-email}",
-    description="{short-description}",
-    license='MIT',
+    author="Tom Elliott",
+    author_email="tom.elliott@nyu.edu",
+    description="Interact with digital gazetteers",
+    license="AGPL-3.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    {project-url}
+    url="https://github.com/isawnyu/apographe",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: {pyver}",
+        "Programming Language :: Python :: 3.10.2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['airtight'],
-    python_requires='>={pyver}'
+    install_requires=[],
+    python_requires="==3.10.2",
 )
